bf4282fc07e4052adc730a67481138792b1e3851
FabianoInocencio1 <fa.inocencio28@gmail.com>
2025-02-10 02:41:33 +0000

    adicionando o aventureiro

---
+++ aventureiroxadrez.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+   
+   
+printf ("movimento do Cavalo\n");
+printf ("\n");
+
+for (int i= 0; i < 2; i++){
+        printf ("Para baixo \n");
+    }
+ 
+    
+    int j=1;
+    
+    while (j <= 1){
+        printf("Para Esquerda\n");
+        j++;
+    }
+   
+    return 0;
+}
